58c4902a97f26cd4f84b129aad2bbb00b116d5a6
Samara Flor <samaraflor2013>
2023-08-03 11:37:34 -0300

    adicionando arquivos, conexão com banco e makefile

---
+++ app.js
@@ -0,0 +1,51 @@
+const express = require('express')
+const database = require('./configs/database')
+
+// conexão com o banco de dados
+database.mongoose
+  .connect(database.url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }).then(() => {
+    console.log("Conexão estabelecida com o banco de dados.");
+  }).catch(err => {
+    console.log("Não conseguiu conectar com o banco de dados.", err);
+    process.exit();
+  });
+
+// criando uma aplicação express
+const app = express();
+
+// definindo que vamos enviar e receber JSON na requisição
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+// definindo nossas rotas
+// var promocaoRoute = require('./routes/promocaoRoute');
+// app.use('/', promocaoRoute);
+
+// var clienteRoute = require('./routes/clienteRoute');
+// app.use('/', clienteRoute);
+
+// var funcionarioRoute = require('./routes/funcionarioRoute');
+// app.use('/', funcionarioRoute);
+
+// var carroRouter = require('./routes/carroRouter');
+// app.use('/', carroRouter);
+
+// var esportivoRouter = require('./routes/esportivoRouter');
+// app.use('/', esportivoRouter);
+
+// var utilitarioRouter = require('./routes/utilitarioRouter');
+// app.use('/', utilitarioRouter);
+
+// var reservaRoute = require('./routes/reservaRoute');
+// app.use('/', reservaRoute);
+
+// definindo nossa porta e host
+const PORT = 3000;
+const HOST = '0.0.0.0';
+
+app.listen(PORT, HOST, () => {
+  console.log(`Servidor executando na porta`);
+});
---
+++ configs/database.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+mongoose.Promise = global.Promise;
+
+const db = {};
+db.mongoose = mongoose;
+if(envConfig.config.db.tls === true){
+    let key = fs.readFileSync("C:/Users/MyUsername/Path/mongodb.pem");
+    let crt = fs.readFileSync("C:/Users/MyUsername/Path/mongodb.crt");
+    mongoose.connect("mongodb://localhost:27017/localizadb?ssl=true",{
+        server:{
+            "sslValidate": true,
+            "sslKey": key,
+            "sslCert": crt, // if you have one certificate you can use `sslCert` parameter
+        }
+    });
+   }else{
+     mongoose.connect(`mongodb://${dbUrl}`,options);
+   }
+
+module.exports = db;
